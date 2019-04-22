615048af7d3156183742c72e860fdd2751b437f9
Alexander Chudnov <halfer.locker@yandex.ru>
2019-04-22 15:51:54 +0700

    Add project files.

---
+++ TestApp/TestApp/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestApp
+{
+    
+    public partial class MainPage : ContentPage
+    {        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCalculateButton(object sender, EventArgs e)
+        {
+            try
+            {
+                int numberOfWaves = Int32.Parse(numberOfWavesEntry.Text);
+
+                if (numberOfWaves <= 30 && numberOfWaves >= 5)
+                {
+                    await Navigation.PushAsync(new OutputPage(numberOfWaves));
+
+                    numberOfWavesEntry.Text = String.Empty;
+                }
+            }
+            catch (FormatException)
+            {
+
+            }            
+        }
+    }
+}
