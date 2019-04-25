b921bfb98246c51f549d25975ee76bbd6d73956e
Alexander Chudnov <halfer.locker@yandex.ru>
2019-04-25 11:09:45 +0700

    Удалена страница, удален 1 свитч и изменены названия других свитчей

--- TestApp/TestApp/MainPage.xaml.cs
+++ TestApp/TestApp/MainPage.xaml.cs
@@ -3,37 +3,108 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
 
 namespace TestApp
 {
-    
     public partial class MainPage : ContentPage
-    {        
+    {
+        private int _numberOfWaves = 5;
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent();        
         }
 
-        private async void OnCalculateButton(object sender, EventArgs e)
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
         {
-            try
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            int centerOfWidth = info.Width / 2;
+            int centerOfHeight = info.Height / 2;
+
+            canvas.Clear();
+
+            SKPaint blackPaint = new SKPaint
             {
-                int numberOfWaves = Int32.Parse(numberOfWavesEntry.Text);
+                Style = SKPaintStyle.Fill,
+                Color = SKColors.Black
+            };
 
-                if (numberOfWaves <= 30 && numberOfWaves >= 5)
+            // Чертим оси x и y
+            // Ось x
+            canvas.DrawLine(0, centerOfHeight, info.Width, centerOfHeight, blackPaint);
+
+            // Ось y
+            canvas.DrawLine(centerOfWidth, 0, centerOfWidth, info.Height, blackPaint);
+
+            if (TrigonometricSwitch.IsToggled)
+            {                
+                // График не в нуле
+                // sin(x)
+                for (int i = 0; i < info.Width; i++)
                 {
-                    await Navigation.PushAsync(new OutputPage(numberOfWaves));
+                    float PI = (float)(Math.PI);
+                    int x = i;
+                    float y = (float)(info.Height / 9 * (Math.Sin(i * _numberOfWaves * PI / (info.Width - 1))));
 
-                    numberOfWavesEntry.Text = String.Empty;
+                    canvas.DrawCircle(x, info.Height / 2 + y, 2, blackPaint);
+                }
+
+                // cos(x)
+                for (int i = 0; i < info.Width; i++)
+                {
+                    float PI = (float)(Math.PI);
+                    int x = i;
+                    float y = (float)(info.Height / 9 * (Math.Cos(i * _numberOfWaves * PI / (info.Width - 1))));
+
+                    canvas.DrawCircle(x, info.Height / 2 + y, 2, blackPaint);
                 }
             }
-            catch (FormatException)
-            {
 
-            }            
+            if (OtherSwitch.IsToggled)
+            {
+                // График не в нуле
+                // cos(x)
+                for (int i = 0; i < info.Width; i++)
+                {
+                    float PI = (float)(Math.PI);
+                    int x = i;
+                    float y = (float)(info.Height / 9 * (Math.Cos(i * _numberOfWaves * PI / (info.Width - 1))));
+
+                    canvas.DrawCircle(x, info.Height / 2 + y, 2, blackPaint);
+                }
+            }
+            
         }
+
+        private void OnTrigonometricSwitchToggled(object sender, EventArgs e)
+        {
+            if (TrigonometricSwitch.IsToggled)
+            {
+                TrigonometricSwitch.IsEnabled = false;
+                OtherSwitch.IsEnabled = true;
+
+                OtherSwitch.IsToggled = false;
+
+                OutputCanvasView.InvalidateSurface();
+            }
+        }
+
+        private void OnOtherSwitchToggled(object sender, EventArgs e)
+        {
+            if (OtherSwitch.IsToggled)
+            {
+                OtherSwitch.IsEnabled = false;
+                TrigonometricSwitch.IsEnabled = true;
+
+                TrigonometricSwitch.IsToggled = false;
+
+                OutputCanvasView.InvalidateSurface();
+            }
+        }        
     }
 }
