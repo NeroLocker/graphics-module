3ba79be03218c11d93a72e9a1260b40403f1072f
Alexander Chudnov <halfer.locker@yandex.ru>
2019-04-26 16:21:44 +0700

    Добавлен класс красок

--- TestApp/TestApp/MainPage.xaml.cs
+++ TestApp/TestApp/MainPage.xaml.cs
@@ -17,68 +17,136 @@ namespace TestApp
             InitializeComponent();        
         }
 
+        /// <summary>
+        /// Отрисовывает графику на плоскости
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
         {
             SKImageInfo info = args.Info;
             SKSurface surface = args.Surface;
             SKCanvas canvas = surface.Canvas;
 
-            int centerOfWidth = info.Width / 2;
-            int centerOfHeight = info.Height / 2;
+            float centerOfWidth = info.Width / 2;
+            float centerOfHeight = info.Height / 2;
+
+            int thicknessOfLines = 2;
+
+            MyPaints paints = new MyPaints();
 
             canvas.Clear();
 
-            SKPaint blackPaint = new SKPaint
-            {
-                Style = SKPaintStyle.Fill,
-                Color = SKColors.Black
-            };
-
             // Чертим оси x и y
             // Ось x
-            canvas.DrawLine(0, centerOfHeight, info.Width, centerOfHeight, blackPaint);
+            canvas.DrawLine(0, centerOfHeight, info.Width, centerOfHeight, paints.Dict["Black Paint"]);
+
+            // Точка 0
+            canvas.DrawText("0", 0, (float)(info.Height / 2), paints.Dict["Text Paint"]);
+
+            // Пометка оси X
+            canvas.DrawText("x", (float)(info.Width - 24), (float)(info.Height / 2), paints.Dict["Text Paint"]);
 
             // Ось y
-            canvas.DrawLine(centerOfWidth, 0, centerOfWidth, info.Height, blackPaint);
+            //canvas.DrawLine(centerOfWidth, 0, centerOfWidth, info.Height, blackPaint);
 
             if (TrigonometricSwitch.IsToggled)
-            {                
+            {
                 // График не в нуле
                 // sin(x)
+
+                // Выводим надпись
+                canvas.DrawText("sin(x)", 0, (float)(info.Height * 0.25), paints.Dict["Text Paint"]);
+
                 for (int i = 0; i < info.Width; i++)
                 {
                     float PI = (float)(Math.PI);
                     int x = i;
                     float y = (float)(info.Height / 9 * (Math.Sin(i * _numberOfWaves * PI / (info.Width - 1))));
 
-                    canvas.DrawCircle(x, info.Height / 2 + y, 2, blackPaint);
+                    canvas.DrawCircle(x, info.Height / 2 + y, thicknessOfLines, paints.Dict["Black Paint"]);
                 }
 
                 // cos(x)
+
+                paints.Dict["Text Paint"].Color = SKColors.Red;
+                canvas.DrawText("cos(x)", 0, (float)(info.Height * 0.30), paints.Dict["Text Paint"]);
                 for (int i = 0; i < info.Width; i++)
                 {
                     float PI = (float)(Math.PI);
                     int x = i;
                     float y = (float)(info.Height / 9 * (Math.Cos(i * _numberOfWaves * PI / (info.Width - 1))));
 
-                    canvas.DrawCircle(x, info.Height / 2 + y, 2, blackPaint);
+                    canvas.DrawCircle(x, info.Height / 2 + y, thicknessOfLines, paints.Dict["Red Paint"]);
+                }
+
+                // sin(x) + cos(x)
+                paints.Dict["Text Paint"].Color = SKColors.Blue;
+                canvas.DrawText("sin(x) + cos(x)", 0, (float)(info.Height * 0.35), paints.Dict["Text Paint"]);
+                
+                for (int i = 0; i < info.Width; i++)
+                {
+                    float PI = (float)(Math.PI);
+                    int x = i;
+                    float sinY = (float)(info.Height / 9 * (Math.Sin(i * _numberOfWaves * PI / (info.Width - 1))));
+                    float cosY = (float)(info.Height / 9 * (Math.Cos(i * _numberOfWaves * PI / (info.Width - 1))));
+                    float y = sinY + cosY;
+
+                    canvas.DrawCircle(x, info.Height / 2 + y, thicknessOfLines, paints.Dict["Blue Paint"]);
                 }
             }
 
             if (OtherSwitch.IsToggled)
             {
                 // График не в нуле
-                // cos(x)
-                for (int i = 0; i < info.Width; i++)
-                {
-                    float PI = (float)(Math.PI);
-                    int x = i;
-                    float y = (float)(info.Height / 9 * (Math.Cos(i * _numberOfWaves * PI / (info.Width - 1))));
+                // y = kx + b
 
-                    canvas.DrawCircle(x, info.Height / 2 + y, 2, blackPaint);
+                canvas.DrawText("kx", 0, (float)(info.Height * 0.25), paints.Dict["Text Paint"]);
+
+                for (int i = -300; i < 600; i++)
+                {
+                    int x = i;
+                    float y = -x;
+
+                    canvas.DrawCircle((float)(info.Width * 0.35 + x), info.Height / 2 + y, thicknessOfLines, paints.Dict["Black Paint"]);
                 }
+
+                // y = x^2
+
+                paints.Dict["Text Paint"].Color = SKColors.Red;
+                canvas.DrawText("x*x", 0, (float)(info.Height * 0.30), paints.Dict["Text Paint"]);
+                for (int i = -300; i < 300; i++)
+                {
+                    int x = i;
+                    float y = (float)(-0.01 * (x * x));                   
+
+                    canvas.DrawCircle((float)(info.Width * 0.35 + x), info.Height / 2 + y, thicknessOfLines, paints.Dict["Red Paint"]);
+                }
+
+                // y = exp^x
+
+                paints.Dict["Text Paint"].Color = SKColors.Blue;
+                canvas.DrawText("e^x", 0, (float)(info.Height * 0.35), paints.Dict["Text Paint"]);
+
+                double counter = -(info.Width * 0.35);
+                while (counter < 30)
+                {
+                    double x = counter;
+                    float y = (float)(-Math.Exp(x));
+
+                    canvas.DrawCircle((float)(info.Width * 0.35 + x), info.Height / 2 + y, thicknessOfLines, paints.Dict["Blue Paint"]);
+
+                    counter += 0.05;
+                }
+                //for (int i = -300; i < 300; i++)
+                //{
+                //    int x = i;
+                //    float y = (float)(0.0005 * Math.Exp(x));
+
+                //    canvas.DrawCircle((float)(info.Width * 0.25 + x), info.Height / 2 + y, thicknessOfLines, bluePaint);                   
+                //}
+
             }
-            
         }
 
         private void OnTrigonometricSwitchToggled(object sender, EventArgs e)
---
+++ TestApp/TestApp/MyPaints.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Класс, хранящий коллекцию красок
+    /// </summary>
+    class MyPaints
+    {
+
+        public Dictionary<string, SKPaint> Dict = new Dictionary<string, SKPaint>();
+
+        /// <summary>
+        /// Инициализирует словарь красок
+        /// </summary>
+        public MyPaints()
+        {
+            SKPaint blackPaint = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = SKColors.Black
+            };
+
+            SKPaint redPaint = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = SKColors.Red
+            };
+
+            SKPaint bluePaint = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = SKColors.Blue
+            };
+
+            SKPaint textPaint = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = SKColors.Black,
+                TextSize = 32
+            };
+
+            Dict.Add("Black Paint", blackPaint);
+            Dict.Add("Red Paint", redPaint);
+            Dict.Add("Blue Paint", bluePaint);
+            Dict.Add("Text Paint", textPaint);
+        }
+    }
+}
