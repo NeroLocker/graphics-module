06c8c915214ca0a7e097946f7ab05e7d41666fe7
Alexander Chudnov <halfer.locker@yandex.ru>
2019-05-27 18:23:07 +0700

    Добавлен класс графика

---
+++ GraphicsModule/GraphicsModule/Plot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphicsModule
+{
+    /// <summary>
+    /// График
+    /// </summary>
+    class Plot
+    {
+    }
+}
