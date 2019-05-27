d6de699d0715d6e4f65a8f7bdddc48e8ce2aa0ed
Alexander Chudnov <halfer.locker@yandex.ru>
2019-05-27 18:50:57 +0700

    Добавлен пустой класс рамки

---
+++ GraphicsModule/GraphicsModule/Frame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphicsModule
+{
+    /// <summary>
+    /// Представляет рамку
+    /// </summary>
+    class Frame
+    {
+    }
+}
