c874fe9a4fc0fa9b619234b555ea96d0ad96b118
Alexander Chudnov <halfer.locker@yandex.ru>
2019-05-27 20:36:35 +0700

    Добавлены свойства, методы и конструктор для класса Frame
    Свойства: координаты верхней левой и нижней правой точки рамки. Методы: расчет нужных координат.

--- GraphicsModule/GraphicsModule/Frame.cs
+++ GraphicsModule/GraphicsModule/Frame.cs
@@ -25,5 +25,84 @@ namespace GraphicsModule
         {
             get; set;
         }
+
+        /// <summary>
+        /// Координата X левой верхней точки
+        /// </summary>
+        public float FirstPointX
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Координата Y левой верхней точки
+        /// </summary>
+        public float FirstPointY
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Координата X нижней правой точки
+        /// </summary>
+        public float SecondPointX
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Координата Y нижней правой точки
+        /// </summary>
+        public float SecondPointY
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Инициализирует свойства
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="margin"></param>
+        public Frame(SKImageInfo info, float margin)
+        {
+            Info = info;
+            Margin = margin;
+        }
+
+        /// <summary>
+        /// Считает координату X верхней левой точки
+        /// </summary>
+        /// <returns></returns>
+        private float CalculateFirstPointX()
+        {
+            return (float)Info.Width * Margin;
+        }
+
+        /// <summary>
+        /// Считает координату Y верхней левой точки
+        /// </summary>
+        /// <returns></returns>
+        private float CalculateFirstPointY()
+        {
+            return (float)Info.Height * Margin;
+        }
+
+        /// <summary>
+        /// Считает координату X нижней правой точки
+        /// </summary>
+        /// <returns></returns>
+        private float CalculateSecondPointX()
+        {
+            return (float)(Info.Width * (1 - 2 * Margin));
+        }
+
+        /// <summary>
+        /// Считает координату Y нижней правой точки
+        /// </summary>
+        /// <returns></returns>
+        private float CalculateSecondPointY()
+        {
+            return (float)(Info.Height * (1 - 2 * Margin));
+        }
     }
 }
