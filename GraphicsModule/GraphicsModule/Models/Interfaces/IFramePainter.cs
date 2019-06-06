73ccb09840e96449a1b6d010247d1892aa362a58
Alexander Chudnov <halfer.locker@yandex.ru>
2019-06-06 12:52:57 +0700

    Добавлен интерфейс для отрисовщиков рамок

---
+++ GraphicsModule/GraphicsModule/Models/Interfaces/IFramePainter.cs
@@ -0,0 +1,15 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphicsModule
+{
+    /// <summary>
+    /// Отрисовщик рамок.
+    /// </summary>
+    public interface IFramePainter
+    {
+        void Paint(Frame frame, SKCanvas canvas);
+    }
+}
