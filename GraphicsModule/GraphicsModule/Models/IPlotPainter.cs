12e1fe1329c5241e3ef375d5976a86c682ce46ab
Alexander Chudnov <halfer.locker@yandex.ru>
2019-06-06 12:29:16 +0700

    Добавлен интерфейс для отрисовщиков графиков

---
+++ GraphicsModule/GraphicsModule/Models/IPlotPainter.cs
@@ -0,0 +1,15 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphicsModule
+{    
+    /// <summary>
+    /// Интерфейс отрисовщика графиков.
+    /// </summary>
+    public interface IPlotPainter
+    {
+        void Paint(Plot plot, SKCanvas canvas);
+    }
+}
